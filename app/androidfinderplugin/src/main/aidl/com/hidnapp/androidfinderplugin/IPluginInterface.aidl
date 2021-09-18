7434e1c6fb20dfe29828a2fc60ee2ae5d6a86fb3
geraskalnas <geraskalnas@gmail.com>
2021-09-18 16:18:29 +0300

    plugin module added plugin interface and plugin service

---
+++ app/androidfinderplugin/src/main/aidl/com/hidnapp/androidfinderplugin/IPluginInterface.aidl
@@ -0,0 +1,8 @@
+// IPluginInterface.aidl
+package com.hidnapp.androidfinderplugin;
+
+// Declare any non-default types here with import statements
+
+interface IPluginInterface {
+    int start(String msgContent);
+}
---
+++ app/androidfinderplugin/src/main/java/com/hidnapp/androidfinderplugin/PluginService.kt
@@ -0,0 +1,24 @@
+package com.hidnapp.androidfinderplugin
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.os.RemoteException
+import com.hidnapp.androidfinderplugin.IPluginInterface
+import kotlin.Throws
+
+abstract class PluginService : Service() {
+    override fun onBind(intent: Intent): IBinder? {
+        return mBinder
+    }
+
+    private val mBinder: IPluginInterface.Stub = object : IPluginInterface.Stub() {
+        @Throws(RemoteException::class)
+        override fun start(msg: String): Int {
+            return run(msg)//defined in plugin
+        }
+    }
+
+    //protected abstract double getResult(double number1, double number2);
+    protected abstract fun run(msgContent: String?): Int
+}
