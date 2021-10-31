9d449aa8ce4af43ee532aee9689954c7e5560a0b
geraskalnas <geraskalnas@gmail.com>
2021-10-31 19:23:51 +0200

    Add torch plugin

--- app/src/main/java/com/hidnapp/androidfinder/TorchPlugin.kt
+++ app/src/main/java/com/hidnapp/androidfinder/TorchPlugin.kt
@@ -1,19 +1,47 @@
 package com.hidnapp.androidfinder
 
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import android.os.SystemClock.sleep
 import com.hidnapp.androidfinderplugin.PluginService
 
 class TorchPlugin : PluginService() {
-    override fun run(msgContent: String?):Int{
-        if(msgContent=="on"){
-            sleep(2000)
-        }else{
+    private val DELAY_MS = 10000
 
+    private var flashLightStatus: Boolean = false
+    private lateinit var ctx: Context
+
+    override fun run(msgContent: String?):Int{
+        ctx = getContext()
+
+        openFlashLight()
+
+        sleep(DELAY_MS.toLong())
+
+        openFlashLight()
+
+
+        return if (flashLightStatus) 1 else 0
+    }
+
+    private fun openFlashLight() {
+        val cameraManager = ctx.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (!flashLightStatus) {
+            try {
+                cameraManager.setTorchMode(cameraId, true)
+                flashLightStatus = true
+
+            } catch (e: CameraAccessException) {
+            }
+        } else {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            } catch (e: CameraAccessException) {
+            }
         }
 
-        get
-
-        if (msgContent==null) return -1
-        return msgContent.toInt()
     }
 }
